19e21146d1f80967ae8b4bfb27507ca9e79c5d96
LuisVillarrealp <luvillarrealpi@uide.edu.ec>
2026-03-20 18:06:44 +0000

    export key

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -2,3 +2,4 @@ uname -a #daba informacion del ordenador
 which gpg #nos da la ubicacion del binario de gpg
 gpg --version #me da la version e informacion adicional 
 gpg --full-generate-key #genero la llave publica y privada
+gpg --armor --export #exportaba la llave publica a un archivo 
