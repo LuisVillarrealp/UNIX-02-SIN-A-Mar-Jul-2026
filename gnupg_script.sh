1359273670d412741985eecb67bbdbbe3a26b195
LuisVillarrealp <luvillarrealpi@uide.edu.ec>
2026-03-27 00:48:49 +0000

    verify

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -11,3 +11,4 @@ echo "Pero si nos pintamos el pelo" > msj_my_friend.txt #create a file to save t
 gpg --output doc_cifrado.txt --encrypt --recipient jonathanmullo100@gmail.com msj_my_friend.txt #encrypt my friend's message
 gpg --decrypt jony_doc_cifrado.txt #decrypt my friend's hidden message
 gpg --clearsign msj_my_friend.txt #puts a signature in the message
+gpg --verify msj_my_friend.txt.asc #verify that the signature is correct 
