3b7032d4f5818fb4be5ffae5ee43219c39b253d0
LuisVillarrealp <luvillarrealpi@uide.edu.ec>
2026-04-02 13:19:31 +0000

    wip: firmware

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -18,3 +18,13 @@ lsblk -f
 &&
 #Prints a separation to make the informartion easy to understand
 echo -e "\n---\n"
+#tests if the directory /sys/firmware/efi exists, if it exists prints UEFI, if it doesn’t exist, prints BIOS
+[-d /sys/firmware/efi] && echo "UEFI" || echo "BIOS"
+#Evaluate, initiate and close a test and are used to test something.
+[]
+#Verify if the given path is a directory
+-d
+#the path to the efi directory
+/sys/firmware/efi
+#means “or” it runs the next command only if the previous one fails.
+||
