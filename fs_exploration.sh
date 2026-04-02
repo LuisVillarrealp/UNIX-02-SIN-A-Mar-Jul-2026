6ff179e6e114cdae072ed9b548cedaacc8e72ad9
LuisVillarrealp <luvillarrealpi@uide.edu.ec>
2026-04-02 13:09:09 +0000

    wip: Disk information

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -8,3 +8,13 @@ sudo apt update
 sudo apt upgrade
 #installs the parted tool for managing disks and partitions
 sudo apt install parted
+#shows detailed disk and partition information, adding separators between each command’s output for clarity.
+sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e "\n---\n"
+#shows all disks and partitions
+sudo parted -l
+#shows disks with filesystem info
+lsblk -f
+#runs the next command only if the previous one worked (and gate)
+&&
+#Prints a separation to make the informartion easy to understand
+echo -e "\n---\n"
