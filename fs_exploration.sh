f4827f87fed1161f7721e3238b165a8bd8592adf
LuisVillarrealp <luvillarrealpi@uide.edu.ec>
2026-04-02 12:51:39 +0000

    wip : parted installation

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -2,3 +2,9 @@
 cd /
 # lists files and adds a symbol to show their type.
 ls -F
+#refreshes the list of available packages
+sudo apt update
+#installs the latest updates for your system
+sudo apt upgrade
+#installs the parted tool for managing disks and partitions
+sudo apt install parted
