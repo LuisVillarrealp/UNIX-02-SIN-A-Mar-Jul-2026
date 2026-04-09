5a2b2d506e13dc5702915f7a044907073bb24351
LuisVillarrealp <luvillarrealpi@uide.edu.ec>
2026-04-09 12:40:53 +0000

    wip: Home Directory

--- script_boot.sh
+++ script_boot.sh
@@ -6,3 +6,19 @@ cd proyecto/
 cd ..
 #changes to the directory named "proyecto" using an absolute path
 cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto
+#changes to the home directory
+cd
+#displays the current working directory (current path)
+pwd 
+#changes to the home directory using the ~ shortcut
+cd ~
+#changes to the /home/codespace directory (absolute path)
+cd /home/codespace
+# shows the value of the HOME environment variable (user's home directory)
+echo $HOME
+# shows the value of the BASH variable (path to the Bash shell)
+echo $BASH
+# shows the PATH variable (directories where executables are searched)
+echo $PATH
+# changes to the home directory using the HOME variable
+cd $HOME
