4a1e4e2d599e9e04528a5f1442f975a159b9527f
LuisVillarrealp <luvillarrealpi@uide.edu.ec>
2026-04-09 12:26:19 +0000

    wip: Relative and absolute paths

---
+++ script_boot.sh
@@ -0,0 +1,8 @@
+# this command creates a new directory also known as folder.
+mkdir
+#changes to the directory named "proyecto" using a relative path
+cd proyecto/
+#changes to the previous (parent) directory 
+cd ..
+#changes to the directory named "proyecto" using an absolute path
+cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto
