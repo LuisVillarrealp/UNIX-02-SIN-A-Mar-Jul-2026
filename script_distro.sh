e65c0e628bd97584830a610aa0b362acf68d6d6c
LuisVillarrealp <luvillarrealpi@uide.edu.ec>
2026-04-09 13:57:07 +0000

    wip: Setup Linux build environment

---
+++ script_distro.sh
@@ -0,0 +1,12 @@
+# Updates the package list from repositories
+sudo apt update
+# Upgrades installed packages to the latest versions
+sudo apt upgrade
+# Installs required development tools and dependencies
+sudo apt install -y git vim make gcc libncurses-dev flex bison bc cpio libelf-dev libssl-dev syslinux dosfstools qemu-system-x86
+# Clones the Linux kernel repository (shallow clone)
+git clone --depth 1 https://github.com/torvalds/linux.git
+# Changes to the linux directory
+cd linux
+# Opens the kernel configuration menu interface
+make menuconfig
