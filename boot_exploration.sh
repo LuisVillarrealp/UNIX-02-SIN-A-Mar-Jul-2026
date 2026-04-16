940ab93f8dc75c5fa007e034a92d5e4bbf361aba
LuisVillarrealp <luvillarrealpi@uide.edu.ec>
2026-04-16 13:27:02 +0000

    wip: Use of chmod

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -20,3 +20,13 @@ man
     #Create a shallow clone with a history truncated to the specified number of commits. Implies --single-branch unless
     #--no-single-branch is given to fetch the histories near the tips of all branches. If you want to clone submodules shallowly, also
     #pass --shallow-submodules.
+#Creates an empty file named script.sh
+touch script.sh
+#Gives execute permission to all users
+chmod +x script.sh
+#Gives execute permission only to the user (owner)
+chmod u+x script.sh
+#Removes read permission from others
+chmod o-r secreto.txt 
+#Gives read and write to user, removes all permissions from group and others
+chmod u+rw,go-rwx privado
