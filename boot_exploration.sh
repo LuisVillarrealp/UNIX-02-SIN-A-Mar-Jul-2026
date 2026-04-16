ddf0380cb200704f16a31f99f7c6c4273f93399e
LuisVillarrealp <luvillarrealpi@uide.edu.ec>
2026-04-16 12:55:29 +0000

    wip: Creating directories with flags names

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -2,3 +2,7 @@
 ls -l -a -h
 #list the files the same as ls -l -a -h
 ls -lah
+#creates a directory literally named "-rf" (the -- prevents it from being treated as an option)
+mkdir -- -rf
+#removes the directory named "-rf" (again, -- avoids interpreting it as an option)
+rmdir -- -rf
