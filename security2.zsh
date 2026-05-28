03c22863688ac04f8b558afa37266967fd7044b4
LuisVillarrealp <luvillarrealpi@uide.edu.ec>
2026-05-28 12:57:28 +0000

    wip: files before the newgrp

--- security2.zsh
+++ security2.zsh
@@ -6,3 +6,9 @@ id -gn
 touch ~/test_grupo_heredado.txt
 ls -la ~/test_grupo_heredado.txt
 #the primary group is the default group assigned to files created by the user.
+#See the current group
+id -gn 
+echo "Grupo actual:$(id -gn)"
+#Create a file before the newgrp
+touch ~/antes_de_newgrp.txt
+ls -la ~/antes_de_newgrp.txt
