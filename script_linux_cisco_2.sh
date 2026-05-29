654bb2999bbb46112e887b21b3873a87bb23a029
LuisVillarrealp <luvillarrealpi@uide.edu.ec>
2026-05-29 22:50:17 +0000

    Course section 12 commands

--- script_linux_cisco_2.sh
+++ script_linux_cisco_2.sh
@@ -7,3 +7,20 @@ cd ~
 dd if=/dev/zero of=/tmp/swapex bs=1M count=50
 # clones one disk to another (dangerous command).
 # dd if=/dev/sda of=/dev/sdb
+#mv command
+# changes to the Documents directory inside the home folder.
+cd ~/Documents
+# moves the file people.csv to the Work directory.
+mv people.csv Work
+# lists the contents of the Work directory.
+ls Work
+# moves multiple files to the School directory.
+mv numbers.txt letters.txt alpha.txt School
+# lists the contents of the School directory.
+ls School
+# lists files in the current directory.
+ls
+# renames animals.txt to zoo.txt.
+mv animals.txt zoo.txt
+# lists files in the current directory to confirm the rename.
+ls
