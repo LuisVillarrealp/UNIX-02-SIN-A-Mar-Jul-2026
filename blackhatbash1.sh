788912c0dea3fbca542a290dd386ff72020c2b82
LuisVillarrealp <luvillarrealpi@uide.edu.ec>
2026-06-04 13:01:46 +0000

    wip: Blach hat bash: Debugging

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,6 +1,17 @@
+#!/usr/bin/env bash
+#Checking Environment Variables
 bash --version
+set -x
 env
+set +x
 echo ${SHELL}
 echo ${RANDOM}
 echo ${UID}
 echo ${OSTYPE}
+#List Process
+ps -e -f
+ps -ef
+df --human-readable
+#bash -r blackhatbash1.sh
+#bash -n blackhatbash1.sh
+#bash -x blackhatbash1.sh
