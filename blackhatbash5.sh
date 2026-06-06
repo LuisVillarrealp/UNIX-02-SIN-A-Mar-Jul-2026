7578708c8a0a596ed3de4ba9fb3143c392187cc5
LuisVillarrealp <luvillarrealpi@uide.edu.ec>
2026-06-06 01:39:35 +0000

    wip: Black hat bash - Redirection Operators

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -4,3 +4,20 @@ top
 # runs top in the background.
 top &
 #kill -9
+touch test && touch test123
+(ls; ps)
+ls; ps; whoami
+lzl || echo "El comando lzl fallo"
+echo "Hello World!" > output.txt
+cat output.txt
+echo "Bye World!" > output.txt
+cat output.txt
+echo "Bye World!" >> output.txt
+cat output.txt
+ls -l / &> stdout_and_stderr.txt
+ls -l / &>> stdout_and_stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt
+cat stdout.txt
+cat stderr.txt 
+lzl 2> error.txt
+cat error.txt
