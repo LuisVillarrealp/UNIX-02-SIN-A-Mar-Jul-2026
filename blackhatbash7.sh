47ffd75fcc2af067f7a5392d9953b38e1bbd55dc
LuisVillarrealp <luvillarrealpi@uide.edu.ec>
2026-06-19 16:42:32 +0000

    wip: Black hat bash-Sed

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -28,3 +28,9 @@ awk 'NR < 10' log.txt
 grep "42.236.10.117" log.txt
 awk '{print $7}' log.txt
 grep "42.236.10.117" log.txt | awk '{print $7}'
+#sed
+sed 's/Mozilla/Godzilla/g' log.txt
+grep "Mozilla" log.txt
+grep "Godzilla" log.txt # no vale ya que no cambio solo mostro en pantalla 
+sed 's/Mozilla/Godzilla/g' log.txt > newlog.txt
+grep "Godzilla" newlog.txt
